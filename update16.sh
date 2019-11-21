9d16fd936d771641bd298e82cbaae1f502e420b7
Serik53 <56282542+Serik53@users.noreply.github.com>
2019-11-21 18:13:30 +0200

    Update update16.sh

--- update16.sh
+++ update16.sh
@@ -4,15 +4,34 @@ set -e
 
 export LC_ALL="en_US.UTF-8"
 
-binary_url="https://github.com/PACGlobalOfficial/PAC/releases/download/035d4df02/pacglobal-035d4df02-lin64.tgz"
-file_name="pacglobal-035d4df02-lin64"
-extension=".tgz"
+if [ "$1" == "--testnet" ]; then
+	pac_rpc_port=7111
+	pac_port=7112
+	is_testnet=1
+else
+	pac_rpc_port=7111
+	pac_port=7112
+	is_testnet=0
+fi
 
+
+arch=`uname -m`
+version="035d4df02"
+old_version="v0.14.0.0"
+base_url="https://github.com/PACGlobalOfficial/PAC/releases/download/${version}"
+if [ "${arch}" == "x86_64" ]; then
+	tarball_name="PACGlobal-${version}-lin64.tar.gz"
+	binary_url="${base_url}/${tarball_name}"
+else
+	echo "PAC Global binary distribution not available for the architecture: ${arch}"
+	exit -1
+fi
+
+echo "###############################"
+echo "#  Remov old binaries   #"		
+echo "###############################"
 echo ""
-echo "#################################################"
-echo "#  Remov old binaries  #"
-echo "#################################################"
-
+echo "Running this script on Ubuntu 16.04 LTS "
 
 sudo ./pacglobal-cli stop
 
@@ -23,26 +42,37 @@ sudo rm -r pacglobald
 sudo rm -r pacglobal-tx
 
 
+
 echo ""
 echo "###############################"
-echo "#      Get/Setup binaries     #"
+echo "#      Get/Setup binaries     #"		
 echo "###############################"
 echo ""
+
+if test -e "${tarball_name}"; then
+	rm -r $tarball_name
+fi
 wget $binary_url
-if test -e "$file_name$extension"; then
-echo "Unpacking PACGlobal distribution"
-	tar -xzvf $file_name$extension
-	rm -r $file_name$extension
-        chmod +x pacglobald
+if test -e "${tarball_name}"; then
+	echo "Unpacking $PAC Global distribution"
+	tar -xvzf $tarball_name
+	chmod +x pacglobald
 	chmod +x pacglobal-cli
-	echo "Binaries were saved to: /root/PACGlobal"
+	rm -r $tarball_name
 else
-	echo "There was a problem downloading the binaries, please try running the script again."
+	echo "There was a problem downloading the binaries, please try running again the script."
 	exit -1
 fi
 
 
-sleep 60
+echo "###############################"
+echo "#      Running the wallet     #"		
+echo "###############################"
+echo ""
+cd ~/
+
+./pacglobald
+sleep 70
 
 is_pac_running=`ps ax | grep -v grep | grep pacglobald | wc -l`
 if [ $is_pac_running -eq 0 ]; then
@@ -53,5 +83,4 @@ fi
 
 ./pacglobal-cli getinfo
 
-echo ""
-echo "Your masternode server is ready!"
+
